e8ad212a2495a98065be1f048cfbbc6f1e1ecc2d
no7dw <no7david@gmail.com>
2018-02-01 17:45:07 +0800

    fix test

--- test/index.js
+++ test/index.js
@@ -1,11 +1,13 @@
-const assert = require('assert');
+const assert = require('assert')
+const ctModel = require('../index')
 describe('get db record', ()=>{
-  it('get one exists record', async() => {
+  it('get one exists record', async(done) => {
     const user_id = '123'
     const r = await ctModel.hasSignedContract(user_id)
     const r2 = await ctModel.getExpandContractByUserId(user_id)
     if(r == true)
-      asset.equal(r2.user_id,user_id)
+      assert.equal(r2.user_id,user_id)
+    done()
   })
 })
 

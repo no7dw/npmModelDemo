f9a3696bcfc74b9f41d1de634bb04b5b5f1b12ea
no7dw <no7david@gmail.com>
2018-02-01 17:52:52 +0800

    fix test pass

--- test/index.js
+++ test/index.js
@@ -1,13 +1,12 @@
 const assert = require('assert')
 const ctModel = require('../index')
 describe('get db record', ()=>{
-  it('get one exists record', async(done) => {
+  it('get one exists record', async() => {
     const user_id = '123'
     const r = await ctModel.hasSignedContract(user_id)
     const r2 = await ctModel.getExpandContractByUserId(user_id)
     if(r == true)
       assert.equal(r2.user_id,user_id)
-    done()
   })
 })
 
